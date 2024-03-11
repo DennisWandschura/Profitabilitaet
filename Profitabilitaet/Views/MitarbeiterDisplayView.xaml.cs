b7d4233bca25ff4efed5dcd107dfd4b0ef357d3f
denwan <dennis.wandschura@gmail.com>
2024-03-11 15:50:35 +0100

    mockup

---
+++ Profitabilitaet/Views/MitarbeiterDisplayView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Profitabilitaet.Views
+{
+    /// <summary>
+    /// Interaktionslogik für MitarbeiterDisplayView.xaml
+    /// </summary>
+    public partial class MitarbeiterDisplayView : UserControl
+    {
+        public MitarbeiterDisplayView()
+        {
+            InitializeComponent();
+        }
+    }
+}
