1a73352a4d9c3f48cdf567ad1faae9ef371cd1b9
denwan <dennis.wandschura@gmail.com>
2024-03-12 16:40:17 +0100

    update für C#

--- Profitabilitaet/Common/Models/Nutzer.cs
+++ Profitabilitaet/Common/Models/Nutzer.cs
@@ -4,24 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Profitabilitaet.Common.Models
-{
-    public class Nutzer {
-        public int Id { get; }
-        public Rolle Rolle { get; }
-        public String Vorname { get; }
-        public String Nachname { get; }
-        public int Plz { get; }
-        public String Ort { get; }
-        public string Strasse { get; }
-        public int Hausnummer { get; }
-        public Geschlecht Geschlecht { get; }
-        public String Telefonnummer { get; }
-        public DateOnly Einstellungsdatum { get; }
+namespace Profitabilitaet.Common.Models;
 
-        public Boolean HatRolle(Rolle rolle)
-        {
-            return false;
-        }
+public class Nutzer {
+    public int Id { get; }
+    public Rolle Rolle { get; }
+    public string Vorname { get; }
+    public string Nachname { get; }
+    public int Plz { get; }
+    public string Ort { get; }
+    public string Strasse { get; }
+    public int Hausnummer { get; }
+    public Geschlecht Geschlecht { get; }
+    public string Telefonnummer { get; }
+    public DateOnly Einstellungsdatum { get; }
+
+    public bool HatRolle(Rolle rolle)
+    {
+        return false;
     }
 }
--- Profitabilitaet/Common/Models/Projekt.cs
+++ Profitabilitaet/Common/Models/Projekt.cs
@@ -7,37 +7,35 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic;
 
-namespace Profitabilitaet.Common.Models
+namespace Profitabilitaet.Common.Models;
+public class Projekt
 {
-    public class Projekt
-    {
-        public int Id { get; }
-        public string Name { get; }
-        public string Description { get; }
-        public Nutzer Leiter { get; }
-        public decimal Auftragswert { get; }
-        public decimal AngezahlterBetrag { get; }
-        public DateOnly Beginn { get; }
-        public DateOnly Ende { get; }
-        public bool IstStorniert { get; private set; }
-        public List<BuchungArbeitszeit> Buchungen { get; }
+    public int Id { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public Nutzer? Leiter { get; }
+    public decimal Auftragswert { get; }
+    public decimal AngezahlterBetrag { get; }
+    public DateOnly Beginn { get; }
+    public DateOnly Ende { get; }
+    public bool IstStorniert { get; private set; }
+    public List<BuchungArbeitszeit> Buchungen { get; }
 
-        public bool Buchen(Nutzer mitarbeiter, int woche, int jahr, int stunden)
+    public bool Buchen(Nutzer mitarbeiter, int woche, int jahr, int stunden)
+    {
+        return false;
+    }
+
+    public bool Stornieren()
+    {
+        if (IstStorniert)
         {
             return false;
         }
-
-        public bool Stornieren()
+        else
         {
-            if (IstStorniert)
-            {
-                return false;
-            }
-            else
-            {
-                IstStorniert = true;
-                return true;
-            }
+            IstStorniert = true;
+            return true;
         }
     }
 }
