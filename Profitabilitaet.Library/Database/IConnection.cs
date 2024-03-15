a62b9ad3b7017c77171814d0db75c843b1f3dcb5
Dennis Wandschura <Dennis.Wandschura@WVSI.EDU.SFFM>
2024-03-15 11:50:23 +0100

    update backend

---
+++ Profitabilitaet.Library/Database/IConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profitabilitaet.Library.Database;
+
+internal interface IConnection
+{
+    public Task<IEnumerable<Nutzer>> GetNutzer();
+    public Task<Nutzer> GetNutzer(int id);
+
+    public Task<IEnumerable<Projekt>> GetProjekte();
+    public Task<Projekt> GetProjekt(int id);
+}
