1d4c3bff8b9e1e3e1fdd844101b1250dcef72a89
Click13 <Markus_H.TT@t-online.de>
2024-03-15 12:10:46 +0100

    Start der Arbeit am DB-Datenmodell

---
+++ Profitabilitaet/Datennodell.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE abteilung(
+	
+);
+CREATE TABLE nutzer(
+	Id int,
+	Rolle string,
+	Vorname string,
+	Nachname string,
+	Plz int,
+	Ort string,
+	Strasse string,
+	Hausnummer int,
+	Geschlecht string,
+	Telefonnummer string,
+	Einstellungsdatum date
+);
+CREATE TABLE projekt(
+	Id int,
+	Name string,
+	Description string,
+	Leiter id,
+	Auftragswert decimal(65,2),
+	AngezahlterBetrag decimal(65,2),
+	Beginn date,
+	Ende date,
+	IstStorniert boolean
+);
